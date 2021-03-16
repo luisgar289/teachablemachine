aca18aaccfa65a717787c92c08e2189a48539ef7
luisgar289 <1720110205@utectulancingo.edu.mx>
2021-03-16 02:48:04 +0000

    Solicitar el nombre del archivo

--- proyecto.py
+++ proyecto.py
@@ -14,18 +14,20 @@ model = tensorflow.keras.models.load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 # Replace this with the path to your image
-image = Image.open('jeonghan7.jpg')
+
+image = input('Introduce el nombre de tu archivo: ')
+imagen = Image.open(image)
 
 #resize the image to a 224x224 with the same strategy as in TM2:
 #resizing the image to be at least 224x224 and then cropping from the center
 size = (224, 224)
-image = ImageOps.fit(image, size, Image.ANTIALIAS)
+imagen = ImageOps.fit(imagen, size, Image.ANTIALIAS)
 
 #turn the image into a numpy array
-image_array = np.asarray(image)
+image_array = np.asarray(imagen)
 
 # display the resized image
-image.show()
+imagen.show()
 
 # Normalize the image
 normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
@@ -37,9 +39,9 @@ data[0] = normalized_image_array
 prediction = model.predict(data)
 
 for i in prediction:
-    if i[0] > 0.6:
+    if i[0] > 0.5:
         print("El Artista de tu imagen es Taylor Swift")
-    elif i[1] > 0.6:
+    elif i[1] > 0.5:
         print("El Artista de tu imagen es Bad Bunny")
-    elif i[2] > 0.6:
+    elif i[2] > 0.5:
         print ("El Artista de tu imagen es JeongHan")
